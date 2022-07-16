fce764242e3df11d7e0e49a41846638b9a8c56f2
aldrian <aldrianalmonte02@gmail.com>
2022-07-15 21:21:21 -0700

    intial commit

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
-from .views import PostListView
+from .views import (
+    PostCreateView,
+    PostListView, 
+    PostDetailView,
+    CreateView
+)
 
 urlpatterns = [
     path('', PostListView.as_view(), name='post_list'),
+    path('<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+    path('new/', PostCreateView.as_view(), name='post_new'),
 ]
+ 
